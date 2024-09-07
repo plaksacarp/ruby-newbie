153a548edcf0539586d05ceb066300032f6a8fe2
plaksacarp <44879709+plaksacarp@users.noreply.github.com>
2024-09-07 11:27:47 +0300

    poem reverse

---
+++ poem.rb
@@ -0,0 +1,7 @@
+poem = "My toast has flown from my hand
+And my toast has gone to the moon.
+But when I saw it on television,
+Planting our flag on Halley's comet,
+More still did I want to eat it."
+
+puts poem.lines.reverse
